399bbe833f041cc50159681f4307800ea8835888
Anton Gritsay <anton@angri.ru>
2012-08-10 17:05:10 +0200

    speedups/geodetic: added distance_to_arc(). geodetic_distance can calculate azimuth

--- speedups/geodeticmodule.c
+++ speedups/geodeticmodule.c
@@ -38,6 +38,22 @@ geodetic__geodetic_distance(double lon1, double lat1, double lon2, double lat2)
 }
 
 
+/*
+ * Calculate the azimuth between two points. Parameters are two pairs
+ * of spherical coordinates in radians.
+ */
+static inline double
+geodetic__azimuth(double lon1, double lat1, double lon2, double lat2)
+{
+    double cos_lat2 = cos(lat2);
+    double true_course = atan2(
+        sin(lon1 - lon2) * cos_lat2,
+        cos(lat1) * sin(lat2) - sin(lat1) * cos_lat2 * cos(lon1 - lon2)
+    );
+    return fmod(2 * M_PI - true_course, 2 * M_PI);
+}
+
+
 static const char geodetic_geodetic_distance__doc__[] = "\n\
     Calculate the geodetic distance between two collections of points,\n\
     following the numpy broadcasting rules.\n\
@@ -53,16 +69,19 @@ geodetic_geodetic_distance(
         PyObject *args,
         PyObject *keywds)
 {
-    static char *kwlist[] = {"lons1", "lats1", "lons2", "lats2", NULL};
+    static char *kwlist[] = {"lons1", "lats1", "lons2", "lats2",
+                             "azimuth", NULL};
 
     PyArrayObject *lons1, *lats1, *lons2, *lats2;
+    unsigned char azimuth;
 
-    if (!PyArg_ParseTupleAndKeywords(args, keywds, "O!O!O!O!", kwlist,
+    if (!PyArg_ParseTupleAndKeywords(args, keywds, "O!O!O!O!b", kwlist,
                 &PyArray_Type, &lons1, &PyArray_Type, &lats1,
-                &PyArray_Type, &lons2, &PyArray_Type, &lats2))
+                &PyArray_Type, &lons2, &PyArray_Type, &lats2, &azimuth))
         return NULL;
 
-    PyArrayObject *op[5] = {lons1, lats1, lons2, lats2, NULL /* distance */};
+    PyArrayObject *op[5] = {lons1, lats1, lons2, lats2,
+                            NULL /* distance or azimuth */};
     npy_uint32 flags = 0;
     npy_uint32 op_flags[5];
     NpyIter_IterNextFunc *iternext;
@@ -90,8 +109,12 @@ geodetic_geodetic_distance(
         double lat1 = *(double *) dataptrarray[1];
         double lon2 = *(double *) dataptrarray[2];
         double lat2 = *(double *) dataptrarray[3];
-        *(double *) dataptrarray[4] = geodetic__geodetic_distance(
-                lon1, lat1, lon2, lat2);
+        if (!azimuth)
+            *(double *) dataptrarray[4] = geodetic__geodetic_distance(
+                    lon1, lat1, lon2, lat2);
+        else
+            *(double *) dataptrarray[4] = geodetic__azimuth(
+                    lon1, lat1, lon2, lat2);
     } while (iternext(iter));
 
     PyArrayObject *result = NpyIter_GetOperandArray(iter)[4];
@@ -237,6 +260,89 @@ geodetic_min_distance(
 }
 
 
+static const char geodetic_distance_to_arc__doc__[] = "\n\
+    Calculate a closest distance between a great circle arc and a point\n\
+    (or a collection of points).\n\
+    \n\
+    distance_to_arc(alons, alats, aazimuths, plons, plats) -> dists\n\
+";
+static PyObject *
+geodetic_distance_to_arc(
+        PyObject *self,
+        PyObject *args,
+        PyObject *keywds)
+{
+    static char *kwlist[] = {"alons", "alats", "aazimuths",
+                             "plons", "plats", NULL};
+
+    PyArrayObject *alats, *alons, *aazimuths, *plons, *plats;
+
+    if (!PyArg_ParseTupleAndKeywords(args, keywds, "O!O!O!O!O!", kwlist,
+                &PyArray_Type, &alons, &PyArray_Type, &alats,
+                &PyArray_Type, &aazimuths,
+                &PyArray_Type, &plons, &PyArray_Type, &plats))
+        return NULL;
+
+    PyArrayObject *op[6] = {alons, alats, aazimuths, plons, plats,
+                            NULL /* distance */};
+    npy_uint32 flags = 0;
+    npy_uint32 op_flags[6];
+    NpyIter_IterNextFunc *iternext;
+    PyArray_Descr *double_dtype = PyArray_DescrFromType(NPY_DOUBLE);
+    PyArray_Descr *op_dtypes[] = {double_dtype, double_dtype,
+                                  double_dtype, double_dtype,
+                                  double_dtype, double_dtype};
+    char **dataptrarray;
+
+    op_flags[0] = op_flags[1] = op_flags[2] \
+                  = op_flags[3] = op_flags[4] = NPY_ITER_READONLY;
+    op_flags[5] = NPY_ITER_WRITEONLY | NPY_ITER_ALLOCATE;
+
+    NpyIter *iter = NpyIter_MultiNew(
+            6, op, flags, NPY_KEEPORDER, NPY_NO_CASTING,
+            op_flags, op_dtypes);
+    Py_DECREF(double_dtype);
+    if (iter == NULL)
+        return NULL;
+
+    iternext = NpyIter_GetIterNext(iter, NULL);
+    dataptrarray = NpyIter_GetDataPtrArray(iter);
+    do
+    {
+        double alon = *(double *) dataptrarray[0];
+        double alat = *(double *) dataptrarray[1];
+        double aazimuth = *(double *) dataptrarray[2];
+        double plon = *(double *) dataptrarray[3];
+        double plat = *(double *) dataptrarray[4];
+
+        double azimuth_to_target = geodetic__azimuth(alon, alat, plon, plat);
+        double dist_to_target = geodetic__geodetic_distance(alon, alat,
+                                                            plon, plat);
+
+        double t_angle = fmod(azimuth_to_target - aazimuth + 2 * M_PI,
+                              2 * M_PI);
+
+        double cos_angle = sin(t_angle) * sin(dist_to_target / EARTH_RADIUS);
+        if (cos_angle < -1)
+            cos_angle = -1;
+        else if (cos_angle > 1)
+            cos_angle = 1;
+
+        *(double *) dataptrarray[5] = (M_PI / 2 - acos(cos_angle)) \
+                                      * EARTH_RADIUS;
+    } while (iternext(iter));
+
+    PyArrayObject *result = NpyIter_GetOperandArray(iter)[5];
+    Py_INCREF(result);
+    if (NpyIter_Deallocate(iter) != NPY_SUCCEED) {
+        Py_DECREF(result);
+        return NULL;
+    }
+
+    return (PyObject *) result;
+}
+
+
 static PyMethodDef GeodeticSpeedupsMethods[] = {
     {"geodetic_distance",
             (PyCFunction)geodetic_geodetic_distance,
@@ -246,6 +352,10 @@ static PyMethodDef GeodeticSpeedupsMethods[] = {
             (PyCFunction)geodetic_min_distance,
             METH_VARARGS | METH_KEYWORDS,
             geodetic_min_distance__doc__},
+    {"distance_to_arc",
+            (PyCFunction)geodetic_distance_to_arc,
+            METH_VARARGS | METH_KEYWORDS,
+            geodetic_distance_to_arc__doc__},
 
     {NULL, NULL, 0, NULL} /* Sentinel */
 };
